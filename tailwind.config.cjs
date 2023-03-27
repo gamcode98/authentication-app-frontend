063b6120ac54776f7e123d50b09369803993095e
Gabriel <gamcode98@gmail.com>
2023-03-27 16:56:03 -0300

    add: colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,15 @@ module.exports = {
     fontFamily: {
       'noto-sans': ['Noto Sans', 'sans-serif']
     },
+    colors: {
+      black: '#333333',
+      gray: '#828282',
+      'light-gray': '##BDBDBD',
+      white: '#fff',
+      blue: '#2F80ED',
+      'light-blue': '#2D9CDB',
+      red: '#EB5757'
+    },
     extend: {}
   },
   plugins: []
